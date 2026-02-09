ba215b076dad99cf02e0c30003e37eb536fe044e
Patrick Cezar <116750047+PorasZarec@users.noreply.github.com>
2026-02-09 11:07:39 +0800

    Coolify Hosting Configuration V2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import path from "path";
-import { fileURLToPath } from "url"; // <--- Import this
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// <--- Add these two lines to recreate __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
